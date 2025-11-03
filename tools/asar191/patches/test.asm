e5c6cda546b8f5b83b131c3d0711028d83dbfc95
elusivesmw <elusivesmw@gmail.com>
2025-11-02 17:07:34 -0800

    messing with jump heights

--- tools/asar191/patches/test.asm
+++ tools/asar191/patches/test.asm
@@ -1,10 +1,33 @@
-!offset_normal = 10
-!offset_spin = -5
+!freeram = $7FA200;
+
+!offset_normal = !freeram
+!offset_spin   = !freeram+1
+
+!debug_out = !freeram+2
+
 ; index based on speed
 ; even - normal jump
 ; odd - spin jump
-org $00D2BD
-db $B0+!offset_normal,$B6+!offset_spin,$AE+!offset_normal,$B4+!offset_spin,$AB+!offset_normal,$B2+!offset_spin,$A9+!offset_normal,$B0+!offset_spin
-db $A6+!offset_normal,$AE+!offset_spin,$A4+!offset_normal,$AB+!offset_spin,$A1+!offset_normal,$A9+!offset_spin,$9F+!offset_normal,$A6+!offset_spin
 
-; TODO: boost jump height
+; params: X contains table index based on speed and jump type
+; return: A with mario's jump height
+org $00D663
+
+autoclean jml get_height
+nop
+
+freecode
+get_height:
+lda.l jump_height,x
+clc : adc !offset_normal
+sta !debug_out
+.return
+sta $7d
+jml $00D667
+
+
+jump_height:
+db $B0,$B6,$AE,$B4,$AB,$B2,$A9,$B0
+db $A6,$AE,$A4,$AB,$A1,$A9,$9F,$A6
+
+; TODO: handle boost jump heights also
