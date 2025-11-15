e83f7229d2c244da20435bbc4dccae1b19f51272
elusivesmw <elusivesmw@gmail.com>
2025-11-15 15:56:19 -0800

    disable pick up throwblock without carry ability
    and some formatting changes :]

--- tools/asar191/patches/ability_carry.asm
+++ tools/asar191/patches/ability_carry.asm
@@ -3,16 +3,28 @@
 ; vars
 !ability_carry = !freeram+3
 
+; disable carrying sprites
 org $01AA5E
+    autoclean jml disable_carry 
+    nop #2
 
-autoclean jml check_carry
-nop #2
+; disable picking up throwblocks
+org $00F26F
+    autoclean jml disable_throwblock 
+    nop #2
 
 freecode
 
-check_carry:
-lda $1470
-ora $187A
-ora !ability_carry
-.return
-jml $01AA64
+disable_carry:
+    lda $1470
+    ora $187A
+    ora !ability_carry
+    ; return
+    jml $01AA64
+
+disable_throwblock:
+    lda $148F
+    ora $187A
+    ora !ability_carry
+    ; return
+    jml $00F275
