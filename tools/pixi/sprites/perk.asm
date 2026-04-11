7378bc5178740688f7107716ebcb4b947f28b09f
elusivesmw <elusivesmw@gmail.com>
2026-04-11 08:04:40 -0700

    add perk sparkle from disassembly

--- tools/pixi/sprites/perk.asm
+++ tools/pixi/sprites/perk.asm
@@ -1,9 +1,11 @@
 ; config
 !this_sprite_num = $AF
+!sparkle        = 1 ; should perk sparkle?
 !perk_count     = 7 ; perks max index + 1
 !inc_amount     = 2 ; how much to increase jump height by
 !jump_max_inc   = 32 ; normal/spin increment max
 !boost_max_inc  = 24 ; boost increment max
+
 
 ; ram setup
 !saveram        = $7FA200
@@ -12,6 +14,7 @@
 !jump_spin      = !saveram+2
 !jump_boost     = !saveram+3
 !disable_carry  = !saveram+4
+
 
 
 print "INIT ",pc
@@ -37,6 +40,9 @@ rts
 
 main:
     jsr graphics
+if !sparkle == 1
+    jsr CODE_01B14E
+endif
 
     ; check if offscreen
     lda #$00
@@ -144,6 +150,75 @@ tile_map_1:
     db $80,$82,$84,$86,$88,$8A,$8C
 tile_map_2:
     db $A0,$A2,$A4,$A6,$A8,$AA,$AC
+
+
+
+
+CODE_01B14E:					;-----------| Sprite glitter subroutine.
+	LDA $13						;$01876A	|\ 
+	AND.b #$1F					;$01876C	|| Make the sphere glitter.
+	ORA $9D						;$01876E	||
+CODE_01B152:					;```````````|
+	ORA.w $186C,X				;$01B152	|\ 
+	ORA $9D						;$01B155	|| Return if game frozen or sprite offscreen.
+	BNE Return01B191			;$01B157	|/
+	JSL $01ACF9					;$01B159	|\ get rand
+	AND.b #$0F					;$01B15D	||
+	CLC							;$01B15F	||
+	LDY.b #$00					;$01B160	||
+	ADC.b #$FC					;$01B162	||
+	BPL CODE_01B167				;$01B164	||
+	DEY							;$01B166	||
+CODE_01B167:					;			||
+	CLC							;$01B167	|| Give a random X offset in the range #$FFFC-#$000B.
+	ADC $E4,X					;$01B168	|| Return if that would stick the sparkle offscreen.
+	STA $02						;$01B16A	||
+	TYA							;$01B16C	||
+	ADC.w $14E0,X				;$01B16D	||
+	PHA							;$01B170	||
+	LDA $02						;$01B171	||
+	CMP $1A						;$01B173	||
+	PLA							;$01B175	||
+	SBC $1B						;$01B176	||
+	BNE Return01B191			;$01B178	|/
+	LDA.w $148E					;$01B17A	|\ 
+	AND.b #$0F					;$01B17D	||
+	CLC							;$01B17F	||
+	ADC.b #$FE					;$01B180	||
+	ADC $D8,X					;$01B182	|| Give a random Y offset in the range #$00FE-#$010D.
+	STA $00						;$01B184	||
+	LDA.w $14D4,X				;$01B186	||
+	ADC.b #$00					;$01B189	||
+	STA $01						;$01B18B	|/
+	JSR CODE_0285BA				;$01B18D	| Spawn the sparkle.
+Return01B191:					;			|
+	RTS							;$01B191	|
+
+
+CODE_0285BA:					;			|
+	LDY.b #$0B					;$0285BA	|\ 
+CODE_0285BC:					;			||
+	LDA.w $17F0,Y				;$0285BC	||
+	BEQ CODE_0285C5				;$0285BF	|| Find an empty minor extended sprite slot. Return if none found.
+	DEY							;$0285C1	||
+	BPL CODE_0285BC				;$0285C2	||
+	RTS							;$0285C4	|/
+
+CODE_0285C5:
+	LDA.b #$05					;$0285C5	|\\ Minor extended sprite to spawn (sparkle).
+	STA.w $17F0,Y				;$0285C7	|/
+	LDA.b #$00					;$0285CA	|\ Clear Y speed?
+	STA.w $1820,Y				;$0285CC	|/
+	LDA $00						;$0285CF	|\ Set Y position.
+	STA.w $17FC,Y				;$0285D1	|/
+	LDA $02						;$0285D4	|\ Set X position.
+	STA.w $1808,Y				;$0285D6	|/
+	LDA.b #$17					;$0285D9	|\\ Number of frames to keep the sprite active for.
+	STA.w $1850,Y				;$0285DB	|/
+	RTS							;$0285DE	|
+
+
+
 
 ; determine random perk
 ; return perk index in A
