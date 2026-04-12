14bcafd4b8a1c1c2786635bf356308e26adc5667
elusivesmw <elusivesmw@gmail.com>
2026-04-12 16:28:01 -0700

    finished poc for perk text

--- tools/uberasm/level/bonus_level.asm
+++ tools/uberasm/level/bonus_level.asm
@@ -1,41 +1,20 @@
+; thanks to Thomas for example and stripe tool:
+; https://www.smwcentral.net/?p=viewthread&t=93207
+; https://jsfiddle.net/ankougo/vgkb8f3m/
+
 incsrc "chars.asm"
 
 ; test
 main:
-    jsr WriteStripe2
+    jsr write_stripe
 rtl
 
-
-StripeTable:
-db $59,$09,$00,$0D ; 0D = 2x7-1
-db 'E',$28,'L',$28,'U',$28,'S',$28,'I',$28,'V',$28,'E',$28
-
-; ty thomas
-WriteStripe:
-    LDY #$00		; counter for how many tiles written
-    lda $7F837B : tax ; get current stripe index
-    .loop
-    LDA StripeTable,y	; copy one byte
-    STA $7F837D,x
-    INX			; increment indices
-    INY
-    CPY #$1A ; compare 2*$0D ; if not at the end of the table, repeat
-    BMI .loop
-
-    LDA #$FF		; write $FF as the ending byte
-    STA $7F837D,x
-    txa : sta $7F837B		; store stripe end index
-    RTS
-
-
-
 stripe_text:
-    db "howdy";
-
-stripe_body:
-    db 'H',$28,'O',$28,'W',$28,'D',$28,'Y',$28
-stripe_body_end:
-!body_size = stripe_body_end-stripe_body
+    db "elusive .,*-!=:"
+stripe_text_end:
+!text_size = stripe_text_end-stripe_text
+!body_size = !text_size*2 ; *2 for the palette bytes
+!palette = $28
 
 stripe_header:
     db $59,$09,$00 ; positioning, etc.
@@ -43,11 +22,9 @@ stripe_header:
 stripe_header_end:
 !header_size = stripe_header_end-stripe_header ; #$04
 
-; ty thomas
-WriteStripe2:
+write_stripe:
     lda $7F837B : tax ; get current stripe index
 
-wdm
     ldy #$00 ; number of header bytes written
     .header:
     lda stripe_header,y ; copy header byte
@@ -57,15 +34,18 @@ wdm
     bmi .header
 
     ; reset y
-    ldy #$00 ; number of tiles/palettes (bytes) written
+    ldy #$00 ; number of tiles written
     .body
+    lda.b stripe_text,y ; copy text byte
+    sta $7F837D,x
+    inx
 
-    ..text
-    lda stripe_body,y ; copy text byte
+    lda.b #!palette ; copy palette byte
     sta $7F837D,x
     inx : iny
-    cpy.b #!body_size;
-    bmi .body_text
+
+    cpy.b #!text_size;
+    bmi .body
 
     lda #$FF ; write $FF as the ending byte
     sta $7F837D,x
