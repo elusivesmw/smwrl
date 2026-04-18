f1a2af98bd7ff1e08a81d010fab23f9ef679b630
elusivesmw <elusivesmw@gmail.com>
2026-04-17 17:50:00 -0700

    wip: header functions

--- tools/uberasm/level/bonus_level.asm
+++ tools/uberasm/level/bonus_level.asm
@@ -13,23 +13,42 @@ incsrc "chars.asm"
 !curr_text = $02 ; scratch (2)
 !curr_len = $04 ; scratch (1)
 
+; EHHHYXyy yyyxxxxx DRLLLLLL llllllll
+; E: End of data. Setting this ignores everything after and ends the upload routine.
+; HHH: VRAM destination - hardcoded to layer 3
+; Yyyyyy: Y position
+; Xxxxxx: X position
+; D: Stripe direction (0 = horizontal, 1 = vertical)
+; R: RLE flag. If clear, the data consists of <length> individual tiles; if set, it consists of a single tile repeated <length> times.
+; LLLLLLllllllll: Length of the data to write.
+;
+; ignore E, HHH, D, R for now
+; EHHHYXyy yyyxxxxx DRLLLLLL llllllll
+function xb(x) = ((x&$FF)<<8)|($FF&(x>>8)) ; swap high and low bytes
+function header1(x,y) = xb($5000|((y<<5)|x)) ; location
+function header2(l) = xb(l) ; just length for now (implied horizontal and non-rle behavior)
+print "asdf ", hex(header1(19,02)), " asdf"
+
 !message_count = 0
-macro stripe_message(label, message)
+macro stripe_message(label,x,y,message)
     !message_count #= !message_count+1
     print "stripe_message ", "<label>", " written at PC: ", pc
     <label>:
-        db $59,$09,$00 ; positioning, etc. ; TODO: parameterize
-        db ((<label>_end-<label>_text)*2)-1; index of body
+        ;db $59,$09,$00 ; positioning, etc. ; TODO: parameterize
+        ;db ((<label>_end-<label>_text)*2)-1; index of body
+
+        dw header1(<x>,<y>) ; position
+        dw header2(((<label>_end-<label>_text)*2)-1) ; length
     <label>_text:
         db "<message>"
     <label>_end:
 endmacro
 
-%stripe_message(jump_ability, "all jumps unlocked")
-%stripe_message(normal_jump, "normal jump height increased")
-%stripe_message(spin_jump, "spin jump height increased")
-%stripe_message(boost_jump, "boost jump height increased")
-%stripe_message(enable_carry, "carrying items now enabled")
+%stripe_message(jump_ability, 19, 2,"all jumps unlocked")
+%stripe_message(normal_jump, 9, 40, "normal jump height increased")
+%stripe_message(spin_jump, 9, 40, "spin jump height increased")
+%stripe_message(boost_jump, 9, 40, "boost jump height increased")
+%stripe_message(enable_carry, 9, 40, "carrying items now enabled")
 print "message_count ", "!message_count"
 
 print "stripe_table written at PC: ", pc
@@ -39,6 +58,8 @@ stripe_table:
     dw spin_jump, spin_jump_text
     dw boost_jump, boost_jump_text
     dw enable_carry, enable_carry_text
+
+
 
 main:
     lda.l !stripe_message_index
