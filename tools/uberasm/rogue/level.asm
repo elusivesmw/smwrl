3add91de59e0989049936dbef031fef0170f24aa
elusivesmw <elusivesmw@gmail.com>
2026-04-25 13:40:33 -0700

    limit bonus game activation
    once per level beaten, so that rebeating a level does not give another bonus game.

--- tools/uberasm/rogue/level.asm
+++ tools/uberasm/rogue/level.asm
@@ -1,7 +1,16 @@
 init:
+    ; TODO: don't set bonus flag until after the level is beaten,
+    ; which means this code will go elsewhere
+
+    ; set bonus game flag if level hasn't been beaten yet
+    lda $13BF : tax ; get translevel number as index
+    lda $1EA2,x : and #$80 ; check level beaten flag
+    bne .return
+
     ; set bonus game flag
-    lda #$01 : sta $7E1425
+    lda #$01 : sta $1425
     ; TODO: other flags related to which bonus room to go to
+    .return:
 rtl
 
 main:
