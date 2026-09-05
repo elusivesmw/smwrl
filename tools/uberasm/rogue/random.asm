a0c85f728f4c20aa44f9a3a6ac5f5c5dbac8868f
elusivesmw <elusivesmw@gmail.com>
2026-09-05 15:25:33 -0700

    option to not clobber rng seed

--- tools/uberasm/rogue/random.asm
+++ tools/uberasm/rogue/random.asm
@@ -1,30 +1,42 @@
 ; copy of vanilla RNG routine at $01ACF9
+; modified to use different input and output addresses, and keep original seed if requested
 !seed1 = !initial_seed
 !seed2 = !seed1+1
-!seed2_copy = $00
-!output = !current_seed 
+!seed1_copy = $00
+!seed2_copy = !seed1_copy+1
+!output = !current_seed
+!mutate = $02 ; 00 - immutable, 01 - mutable 
 
-; clobbers $00 and !seed1,!seed
+; clobbers $00, $01, and optionally !seed1, !seed2
 ; TODO: dont' clobber !seed1,!seed2
 GetRand:                        ;-----------| Random number generation routine. Outputs in !seed1/!ouput (returns !seed2)
     PHx                         ;$01ACF9    |
     LDx.b #$01                  ;$01ACFA    |
-    JSL CODE_01AD07             ;$01ACFC    | Run RNG for high byte.
+    JSL preprocess              ;$01ACFC    | Run RNG for high byte.
     DEx                         ;$01AD00    |
-    JSL CODE_01AD07             ;$01AD01    | Run RNG for low byte.
+    JSL preprocess              ;$01AD01    | Run RNG for low byte.
     PLx                         ;$01AD05    |
     RTL                         ;$01AD06    |
 
-CODE_01AD07:
-    lda !seed2 ; move to scratch for asl and bit operations
+preprocess:
+    ; move to scratch to not mutate input, also no long addressing for asl and bit ops
+    lda !seed1
+    sta !seed1_copy
+    lda !seed2 
     sta !seed2_copy
 
-    LDA !seed1                  ;$01AD07    |\ 
+; INPUT:
+;   X, index of output byte
+;   $02, 00 - immutable, 01 - mutable
+; OUTPUT:
+;   random numbers in !output and !output+1
+CODE_01AD07:
+    LDA !seed1_copy             ;$01AD07    |\ 
     ASL                         ;$01AD0A    ||
     ASL                         ;$01AD0B    || With a = !seed2:
     SEC                         ;$01AD0C    ||  a = 5a + 1;
-    ADC !seed1                  ;$01AD0D    ||
-    STA !seed1                  ;$01AD10    |/ TODO: this currently overwrites inbitial seed, don't want to do this
+    ADC !seed1_copy             ;$01AD0D    ||
+    STA !seed1_copy             ;$01AD10    |/ TODO: this currently overwrites inbitial seed, don't want to do this
     ASL !seed2_copy             ;$01AD13    |\ 
     LDA #$20                    ;$01AD16    ||
     BIT !seed2_copy             ;$01AD18    || With b = !seed2:
@@ -36,9 +48,27 @@ CODE_01AD21:                    ;           ||    b = 2b;
 CODE_01AD23:                    ;           ||
     inc !seed2_copy             ;$01AD23    |/
 CODE_01AD26:                    ;           |
-    LDA !seed2_copy                  ;$01AD26    |\ 
-    sta !seed2 ; restore from copy, TODO: this currently overwrites initial seed, don't want to do this
-    EOR !seed1                  ;$01AD29    || Invert byte B with byte A and output the result.
+    LDA !seed2_copy             ;$01AD26    |\ 
+    EOR !seed1_copy             ;$01AD29    || Invert byte B with byte A and output the result.
     STA !output,x               ;$01AD2C    |/
 
-    RTL                         ;$01AD2F    |
+    ; mutate if input says to
+    lda !mutate : beq .return
+
+    .mutate:
+    lda !seed1_copy : sta !seed1
+    lda !seed2_copy : sta !seed2 
+
+    .return:
+RTL                             ;$01AD2F    |
+
+
+; compare to vanilla
+GetRand2:
+    lda !seed1
+    sta $148B
+    lda !seed2
+    sta $148C
+
+    jsl $01ACF9
+    rtl
